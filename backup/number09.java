13e834d5f783180834f75f8a5fbf3387c7117c93
karezamaulana <karezamaulana@gmail.com>
2025-11-21 18:15:35 +0700

    PERCOBAAN 3

---
+++ backup/number09.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+
+public class number09 {
+    public static void main(String[] args) {
+        int [][] numbers= new int[3][];
+        numbers[0]= new int[5];
+        numbers[1]= new int[3];
+        numbers[2]= new int[1];
+        for (int i=0 ; i<numbers.length; i++){
+            System.out.println(Arrays.toString(numbers[i]));
+        }
+        for (int i=0 ; i<numbers.length; i++){
+            System.out.println("panjang baris ke "+(i+1)+" adalah : "+numbers[i].length);
+        }
+    }
+}
